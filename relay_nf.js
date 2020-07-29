df12a0e0e867d55f4e55101d15a75b8f015d3288
Ian Alves <ian.alves21@gmail.com>
2020-07-29 19:38:26 -0400

    update

--- relay_nf.js
+++ relay_nf.js
@@ -1,24 +1,75 @@
 module.exports = function(RED) {
-
-    "use strict";
+    // "use strict";
     var mapeamentoNode;
 
+    function multipleRelayNF(self, file, slot, currentMode){
+        for(var t=0; t<self.qtdRelayNF; t++){
+            var command_n={
+                type: "relay_modular_V1_0",
+                slot: parseInt(mapeamentoNode.slot),
+                compare: {},
+                method: "set_status_NF",
+                relay_number: parseInt(self.relay_number_n[t]),
+                relay_value: self.relay_nf_value_n[t] === "true" ? true : false,
+                get_output: {},
+            };
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command_n);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[3].jig_test.push(command_n);
+                }
+            }
+        }
+        return file;
+    }
+
     function relay_nfNode(config) {
-
         RED.nodes.createNode(this, config);
-        this.mapeamento = config.mapeamento
-        this.relay_number = config.relay_number
+        this.mapeamento = config.mapeamento;
+        this.relay_number = config.relay_number;
 
-        var node = this
-        var valor_nf = config.relay_nf_value
+        this.qtdRelayNF = config.qtdRelayNF;
+        this.relay_number_n = []; this.relay_nf_value_n = [];
+        this.relay_number_n.push(config.relay_number1); this.relay_nf_value_n.push(config.relay_nf_value1);
+        this.relay_number_n.push(config.relay_number2); this.relay_nf_value_n.push(config.relay_nf_value2);
+        this.relay_number_n.push(config.relay_number3); this.relay_nf_value_n.push(config.relay_nf_value3);
+        this.relay_number_n.push(config.relay_number4); this.relay_nf_value_n.push(config.relay_nf_value4);
+        this.relay_number_n.push(config.relay_number5); this.relay_nf_value_n.push(config.relay_nf_value5);
+        this.relay_number_n.push(config.relay_number6); this.relay_nf_value_n.push(config.relay_nf_value6);
+        this.relay_number_n.push(config.relay_number7); this.relay_nf_value_n.push(config.relay_nf_value7);
+        this.relay_number_n.push(config.relay_number8); this.relay_nf_value_n.push(config.relay_nf_value8);
+        this.relay_number_n.push(config.relay_number9); this.relay_nf_value_n.push(config.relay_nf_value9);
+        this.relay_number_n.push(config.relay_number10); this.relay_nf_value_n.push(config.relay_nf_value10);
+        this.relay_number_n.push(config.relay_number11); this.relay_nf_value_n.push(config.relay_nf_value11);
+        this.relay_number_n.push(config.relay_number12); this.relay_nf_value_n.push(config.relay_nf_value12);
+        this.relay_number_n.push(config.relay_number13); this.relay_nf_value_n.push(config.relay_nf_value13);
+        this.relay_number_n.push(config.relay_number14); this.relay_nf_value_n.push(config.relay_nf_value14);
+        this.relay_number_n.push(config.relay_number15); this.relay_nf_value_n.push(config.relay_nf_value15);
+        this.relay_number_n.push(config.relay_number16); this.relay_nf_value_n.push(config.relay_nf_value16);
+        this.relay_number_n.push(config.relay_number17); this.relay_nf_value_n.push(config.relay_nf_value17);
+        this.relay_number_n.push(config.relay_number18); this.relay_nf_value_n.push(config.relay_nf_value18);
+        this.relay_number_n.push(config.relay_number19); this.relay_nf_value_n.push(config.relay_nf_value19);
+        this.relay_number_n.push(config.relay_number20); this.relay_nf_value_n.push(config.relay_nf_value20);
+        this.relay_number_n.push(config.relay_number21); this.relay_nf_value_n.push(config.relay_nf_value21);
+        this.relay_number_n.push(config.relay_number22); this.relay_nf_value_n.push(config.relay_nf_value22);
+        this.relay_number_n.push(config.relay_number23); this.relay_nf_value_n.push(config.relay_nf_value23);
+        this.relay_number_n.push(config.relay_number24); this.relay_nf_value_n.push(config.relay_nf_value24);
 
-        mapeamentoNode = RED.nodes.getNode(this.mapeamento)
+        var node = this;
+        var valor_nf = config.relay_nf_value;
 
-
-
+        mapeamentoNode = RED.nodes.getNode(this.mapeamento);
         node.on('input', function(msg, send, done) {
-
-            // substitua a variavel msg pela a informação desejada a ser passada via serial
             var globalContext = node.context().global;
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
@@ -30,66 +81,33 @@ module.exports = function(RED) {
                 relay_number: parseInt(node.relay_number),
                 relay_value: valor_nf === "true" ? true : false,
                 get_output: {},
-            }
-            var file = globalContext.get("exportFile")
+            };
+            var file = globalContext.get("exportFile");
             var slot = globalContext.get("slot");
             if(!(slot === "begin" || slot === "end")){
                 if(currentMode == "test"){
                     file.slots[slot].jig_test.push(command);
+                    file = multipleRelayNF(node, file, slot, currentMode);
                 }
                 else{
                     file.slots[slot].jig_error.push(command);
+                    file = multipleRelayNF(node, file, slot, currentMode);
                 }
             }
             else{
                 if(slot === "begin"){
                     file.slots[0].jig_test.push(command);
-                    // file.begin.push(command);
+                    file = multipleRelayNF(node, file, slot, currentMode);
                 }
                 else{
                     file.slots[3].jig_test.push(command);
-                    // file.end.push(command);
+                    file = multipleRelayNF(node, file, slot, currentMode);
                 }
             }
             globalContext.set("exportFile", file);
-            console.log(command)
-             // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
-
-            send(msg)
-
+            console.log(command);
+            send(msg);
         });
-
-        // var nodeDaVariavel = config.NodeDaVariavel
-
     }
-
-    // nome do modulo
     RED.nodes.registerType("relay_nf", relay_nfNode);
-
-    // RED.httpAdmin.get("/statusNF", function(req, res) {
-    //     console.log(mapeamentoNode)
-    //     if (mapeamentoNode) {
-    //         res.json([
-    //             { value: mapeamentoNode.valuePort9, label: "NF9 | CM9 - " + mapeamentoNode.labelPort9, hasValue: false },
-    //             { value: mapeamentoNode.valuePort10, label: "NF10 | CM10 - " + mapeamentoNode.labelPort10, hasValue: false },
-    //             { value: mapeamentoNode.valuePort11, label: "NF11 | CM11 - " + mapeamentoNode.labelPort11, hasValue: false },
-    //             { value: mapeamentoNode.valuePort12, label: "NF12 | CM12 - " + mapeamentoNode.labelPort12, hasValue: false },
-    //             { value: mapeamentoNode.valuePort13, label: "NF13 | CM13 - " + mapeamentoNode.labelPort13, hasValue: false },
-    //             { value: mapeamentoNode.valuePort14, label: "NF14 | CM14 - " + mapeamentoNode.labelPort14, hasValue: false },
-    //             { value: mapeamentoNode.valuePort15, label: "NF15 | CM15 - " + mapeamentoNode.labelPort15, hasValue: false },
-    //             { value: mapeamentoNode.valuePort16, label: "NF16 | CM16 - " + mapeamentoNode.labelPort16, hasValue: false },
-    //         ])
-    //     } else {
-    //         res.json([
-    //             { label: "NF9 | CM9", value: "0", hasValue: false },
-    //             { label: "NF10 | CM10", value: "1", hasValue: false },
-    //             { label: "NF11 | CM11", value: "2", hasValue: false },
-    //             { label: "NF12 | CM12", value: "3", hasValue: false },
-    //             { label: "NF13 | CM13", value: "5", hasValue: false },
-    //             { label: "NF14 | CM14", value: "6", hasValue: false },
-    //             { label: "NF15 | CM15", value: "7", hasValue: false },
-    //             { label: "NF16 | CM16", value: "8", hasValue: false },
-    //         ])
-    //     }
-    // });
-}
+};
